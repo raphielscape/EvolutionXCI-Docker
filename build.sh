7ff292896e7f552498053588d3acec7529adfd61
baalajimaestro <baalajimaestro@raphielgang.org>
2019-10-07 20:11:07 +0900

    SendTG Impl
    Signed-off-by: Akito Mizukito <peak@echln.net>

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+function sendTG() {
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001176435322&parse_mode=Markdown"
+}
+
+sendTG "\`Docker image is being pushed!\`"
+
 docker build . -t harukanetwork/evolutionx-ota-ci:latest
 docker tag harukanetwork/evolutionx-ota-ci:latest harukanetwork/evolutionx-ota-ci:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push harukanetwork/evolutionx-ota-ci
+
+sendTG "@RealAkito \`I have pushed new images to docker\` %0A [Images are Here](https://hub.docker.com/r/harukanetwork/evolutionx-ota-ci)"
